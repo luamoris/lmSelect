4da2cb8ed423ff0a833cf6fd953208ddd4a4aa46
IOSIF ISACHKO <70754306+luamoris@users.noreply.github.com>
2023-04-30 01:42:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ function logData(data) {
 }
 
 const selectorId1 = "formId1";
-const selectorForm1 = new LmSelect(document, selectorId1);
+const selectorForm1 = new LmSelect(selectorId1);
 selectorForm1.init(logData);
 
 const selectorId2 = "formId2";
-const selectorForm2 = new LmSelect(document, selectorId2);
+const selectorForm2 = new LmSelect(selectorId2);
 selectorForm2.init(logData);
